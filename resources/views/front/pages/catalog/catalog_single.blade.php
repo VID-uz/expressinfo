e3d73e9511f4f5d7984a87d2bd48c4fcba9edc76
Asadxoja <asadxoja5651@gmail.com>
2019-08-14 20:13:26 +0500

    New updates

--- resources/views/front/pages/catalog/catalog_single.blade.php
+++ resources/views/front/pages/catalog/catalog_single.blade.php
@@ -86,7 +86,7 @@
                     </h1>
                     <div class="catalog_single_main_info_list">
                         <a href="tel:{{ $item->phone_number }}" class="catalog_single_main_info_list_item">
-                            {{--<i class="fa fa-phone fa-2x"></i>--}}
+                            <i class="fa fa-phone fa-2x"></i>
                             {{ $item->phone_number }}
                         </a>
                         {{--<a href="{{ $item->url }}" class="catalog_single_main_info_list_item">--}}
